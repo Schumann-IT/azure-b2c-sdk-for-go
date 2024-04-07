2ca356c723c8020046390b50861b3162bee58ea7
Jan Schumann <js@schumann-it.com>
2024-04-07 18:36:21 +0200

    feat: upload secret to keyset

---
+++ msgraph/keyset_test.go
@@ -0,0 +1,24 @@
+package msgraph
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_UploadKeySet(t *testing.T) {
+	s := testHelperSetupService(t)
+	kid := fmt.Sprintf("B2C_1A_%s", acctest.RandStringFromCharSet(10, "ABCDEFGHIJKLMNOPQRSTXYZabcdefghijklmnopqrstxyz"))
+	ks, err := s.UploadSecret(kid, "sig", "secret")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, ks)
+
+	// cleanup
+	_ = s.DeletePolicies()
+	_ = s.DeleteKeySet(kid)
+	_ = s.DeleteKeySet(fmt.Sprintf("%s.bak", kid))
+}
